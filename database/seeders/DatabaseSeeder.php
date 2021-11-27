71ac7ccec3a2d8d4aac206ff71428e82bc4b5479
Dinuka-Sandaruwan-Warnasooriya <dinukaswarnasooriya@gmail.com>
2021-11-27 14:40:16 +0530

    faker_data_added

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Insert Data for tables
+
+        $faker = Faker::create();
+        foreach(range(1,10) as $index){
+            DB::table('students')->insert([
+                'name' =>$faker->name,
+                'email' =>$faker->email,
+            ]);
+        }
+
+
+    }
+}
